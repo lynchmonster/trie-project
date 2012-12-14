31395ad5ca986f499d3b9648297fd6a90786acb0
lynchmonster <aarondlynch@gmail.com>
2012-12-13 23:30:15 -0800

    Update Aaron.trie.rb

--- Aaron.trie.rb
+++ Aaron.trie.rb
@@ -1,7 +1,16 @@
-requires
+def my_array
+ 
+  my_array = ["hello", "goodbye", "congrats"]
+  my_array.each do |x|
+    x = split ''
+    print = "#{my_array}"
+    end
 
+end
+  
 
-def x 
-
-# steps: create a key/array
-# 
+# step 1: create a key/array
+# step 2: split words into individual letters to index them.
+# step 3: assign values to words to assign values to them.
+# step 4: make sure that the values are not determined by the number of letters in the string
+# step 4: have the program sort the words based on their values.
